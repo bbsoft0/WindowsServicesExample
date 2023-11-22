3a093c94f9a936573bfe7bef3b4335907e010bef
Barbu <bbsoft0@gmail.com>
2023-11-22 16:22:42 +0200

    [services] Added Windows Services base functionality

---
+++ FileConverterService/ConverterService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf.Logging;
+
+namespace FileConverterService
+{
+    internal class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+        private static readonly LogWriter _log = HostLogger.Get<ConverterService>();
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher();
+            _watcher.Path = @"C:\temp\ServiceToUpper";
+            _watcher.Filter = "*.txt";
+            _watcher.Created += FileCreated;
+            _watcher.IncludeSubdirectories = false;
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+        public bool Pause()
+        {
+            _watcher.EnableRaisingEvents = false;
+            return true;
+        }
+
+        public bool Continue()
+        {
+            _watcher.EnableRaisingEvents = true;
+            return true;
+        }
+
+        public void CustomCommand(int commandNumber)
+        {
+            _log.InfoFormat("Received command number {0}", commandNumber);
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _log.InfoFormat("Started conversion of : {0}", e.FullPath);
+            if (e.FullPath.Contains("bad_in"))
+            {
+                throw new NotSupportedException("Cannot convert");
+            }
+            string content = File.ReadAllText(e.FullPath);
+            string upperContent = content.ToUpper();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertedFileName = Path.GetFileNameWithoutExtension(e.FullPath) + ".converted";
+            var convertedPath = Path.Combine(dir, convertedFileName);
+            File.WriteAllText(convertedPath, upperContent);
+        }
+
+        public bool Stop()
+        {
+            _watcher.EnableRaisingEvents = false;
+            _watcher.Dispose();
+            return true;
+        }
+    }
+}
---
+++ FileConverterService/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace FileConverterService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.UseNLog();
+
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                    serviceInstance.WhenPaused(execute => execute.Pause());
+                    serviceInstance.WhenContinued(execute => execute.Continue());
+                    serviceInstance.WhenCustomCommandReceived((execute, hostControl, commandNumber) => execute.CustomCommand(commandNumber));
+                });
+
+                serviceConfig.EnableServiceRecovery(recoveryOption =>
+                {
+                    recoveryOption.RestartService(1);   //1 miunte delay    
+                    recoveryOption.RestartComputer(60,"PS Demo");
+                    recoveryOption.RunProgram(5, @"C:\\Windows\\System32\\notepad.exe");
+                    recoveryOption.SetResetPeriod(1);
+                });
+
+                serviceConfig.EnablePauseAndContinue();
+                serviceConfig.SetServiceName("DemoFileConverterService");
+                serviceConfig.SetDisplayName("Demo File Converter Service");
+                serviceConfig.SetDescription("This service converts files from lower case to upper case.");
+                serviceConfig.StartAutomatically();
+
+
+            });
+        }
+    }
+}
